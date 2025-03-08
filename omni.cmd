f8f02c267736876fb4c72446c2168730cf5596bc
Leonardo Farias Bona <leonardofariasbona@gmail.com>
2025-03-07 23:06:42 -0300

    Improvements to commands and changes to names

--- omni.cmd
+++ omni.cmd
@@ -4,36 +4,56 @@ if "%~1"=="/?" (
     type "%~dp0omni-help.md"
     exit /b 0
 )
+
+
+set omni=%userprofile%\.omni\
+if not exist %omni% mkdir %omni%
+if not defined alias_list set alias_list=%omni%.env
+
+:: Open alias list in editor when no argument is provided
+if ""=="%~1" (
+    nvim %alias_list%
+    exit /b 0
+)
+
+:: Initialize variables based on positional arguments
 set destination_arg=%~1
 set option=%2
 set option_extras=%3
-if not defined option set option=.cmd
 
 set temp_dest=%temp%\dest.temp
-if not defined directories_env set directories_env=%userprofile%\.noir\directories.env
+
 :: Isolate Locals
 setlocal EnableDelayedExpansion
 call :loadenvs
 if defined dest_%destination_arg% echo !dest_%destination_arg%! > %temp_dest%
 endlocal
 :: Isolate 
+
 if exist %temp_dest% set /p destination=<%temp_dest% & del %temp_dest%
+
 if not defined destination (
-  echo _%~n1=%~1>>%directories_env%
-  nvim %directories_env%
-  exit /b 0
+    echo _%~n1=%~1>>%alias_list%
+    nvim %alias_list%
+    exit /b 0
 )
 
 mkdir %destination% 2>nul
 pushd %destination%
 
 
-if /i "%option:~0,2%"=="-s" (
+if /i "%option%"=="-s" (
     start . & goto :EOF
 )
 
-if /i "%option:~0,2%"=="-n" (
+if /i "%option%"=="-n" (
     nvim . 
+    popd
+    goto :EOF
+)
+
+if /i "%option%"=="-c" (
+    cd | clip
     popd
     goto :EOF
 )
@@ -46,8 +66,8 @@ if /i "%option%"=="\" (
   goto :searchFile
 )
 
-if not "%option:~0,1%"=="." (
-    nvim "%option%"
+if /i "%option%"=="-f" (
+    nvim "%option_extras%"
     popd
     goto :EOF
 )
@@ -73,7 +93,7 @@ goto :EOF
 
 :: Loadenv function
 :loadenvs
-for /f "tokens=1,2 delims==" %%a in ('findstr "=" %directories_env%') do (
+for /f "tokens=1,2 delims==" %%a in ('findstr "=" %alias_list%') do (
     set "dest_%%a=%%b"
 )
 goto :EOF
